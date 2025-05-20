76ffa7eaf855b3243aa6bc9b12ec5e1837278210
UserThwinmoe <thwinmoemyint@xubuntu2.myguest.virtualbox.org>
2025-05-20 21:40:47 +0630

    complete all js logics only entry is needed

--- src/code.js
+++ src/code.js
@@ -9,9 +9,6 @@ export function addProject() {
     return projectName;
 }
 
-const projectName = addProject();
-addHeadline(projectName);
-
 export function addHeadline(projectName) {
     const headline = document.createElement('h1');
     headline.className = 'p-name';
@@ -19,5 +16,38 @@ export function addHeadline(projectName) {
     headline.textContent = userInput || 'Default Project Name';
 
     projectName.appendChild(headline);
+}
+
+export function taskList(projectName) {
+    const createtaskList = document.createElement('ul');
+    taskList.className = 'lists';
+    projectName.appendChild(createtaskList);
+
+    return createtaskList;
+}
+
+
+export function addListButton(projectName) {
+    const addListButton = document.createElement('button');
+    addListButton.className = 'add-list';
+    addListButton.textContent = '+ Add List';
+    projectName.appendChild(addListButton);
+}
+
+export function addTask(createtaskList) {
+    const taskBox = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    const taskName = document.createElement('span');
+    taskName.className = 'task';
+    const userInput = prompt('Enter the task name:');
+    taskName.textContent = userInput || 'Default Task Name';
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '✖';
+    taskBox.appendChild(checkbox);
+    taskBox.appendChild(taskName);
+    taskBox.appendChild(deleteButton);
+
+    createtaskList.appendChild(taskBox);
 }
 
